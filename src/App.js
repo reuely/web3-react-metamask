c63b6b2a58050d859aecb64e710a2e22bc3e9519
Reuel Yang <reuel@berkeley.edu>
2022-08-17 15:45:28 -0700

    dapp:// deeplink change (might not work)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+//bootstraps
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+//css styling
 import './App.css';
+//kbean logo
 import logo from './assets/kbean-logo.png';
+//useEffect and useState methods from react
 import { useEffect, useState } from "react";
 import web3 from "./web3";
 import { connectWallet, getCurrentWalletConnected } from "./utils/interact.js";
@@ -15,8 +19,8 @@ function App() {
   const [userBalance, setBalance] = useState("");
   const [connectMessage, setMessage] = 
     useState("If there is no Metamask pop-up for 15 seconds after clicking the button, click the metamask extension and make sure you're signed in!");
-  // const [name, setName] = useState("");
-  // const [signer, setSigner] = useState("");
+  // const [name, setName] = useState(""); <-for future uses (maybe)
+  // const [signer, setSigner] = useState(""); <-for future uses (maybe)
 
   //Keeps the Connect button say "Connected!" if the user reloads
   //the page after connecting their wallet.
@@ -31,11 +35,10 @@ function App() {
 
   //Prompts the users to connect Metamask to the website
   const connectWalletPressed = async () => {
-    if (!window.ethereum) { //this conditional is not working actually check if the user is logged into metamask
+    if (!window.ethereum) {
       alert('Install Metamask extension first!');
     } else {
         const walletResponse = await connectWallet();
-        // add a code that alerts user if they reject to connect
         setStatus(walletResponse.status);
         setWallet(walletResponse.address);
       }
@@ -44,7 +47,7 @@ function App() {
   //Displays users' ETH amount on screen
   //(Doesn't account for other coins other than ETH)
   const readWalletInfo = async() => {
-    if (!(walletAddress.length > 0)) { //switch the if to actually check if metmamask is connected
+    if (!(walletAddress.length > 0)) {
       alert('Connect to your Metamask Wallet first!');
     } else {
       const walletBalance = await web3.eth.getBalance(walletAddress);
@@ -54,7 +57,8 @@ function App() {
 
   //Connects to deeplink Metamask mobile app
   function mobilePressed() {
-    const url = "https://metamask.app.link/dapp/reuely.github.io/web3-react-metamask/";
+    //const url = "https://metamask.app.link/dapp/reuely.github.io/web3-react-metamask/";
+    const url = "dapp://reuely.github.io/web3-react-metamask/";
     window.location.replace(url);
   };
 
